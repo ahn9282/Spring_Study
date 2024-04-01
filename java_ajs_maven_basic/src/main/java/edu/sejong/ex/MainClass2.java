083f3436e8661e193c47785e426265ee7fe438b2
cajunSub <junsyub@gmail.com>
2024-04-01 16:40:00 +0900

    스프링 수업 공부

--- java_ajs_maven_basic/src/main/java/edu/sejong/ex/MainClass2.java
+++ java_ajs_maven_basic/src/main/java/edu/sejong/ex/MainClass2.java
@@ -17,11 +17,35 @@ public class MainClass2 {
 		  System.out.println("나의 이름 : "+myInfo.getName());
 		  System.out.println("나의 체중 : "+myInfo.getWeight());
 		  System.out.println("나의 키 : "+myInfo.getHeight()); System.out.print("나의 취미 : " + myInfo.getHobbys());
-		 
-
 		
 		myInfo.getInfo();
-
+		ctx = new GenericXmlApplicationContext("classpath:appCTX3.xml");
+		Pencil pencil =(Pencil) ctx.getBean("pencil",Pencil6B.class);
+		pencil.use();
+		
+		ctx = new GenericXmlApplicationContext("classpath:appCTX4.xml","classpath:appCTX5.xml");
+		Student student1 = (Student)ctx.getBean("student1");
+		System.out.println(student1.getName());
+		System.out.println(student1.getHobbys());
+		StudentInfo studentInfo = (StudentInfo)ctx.getBean("studentInfo1");
+		Student student2  = studentInfo.getStudent();
+		
+		Student student3 = (Student)ctx.getBean("student2");
+		if(student1 == student2) {
+			System.out.println("같습");
+		}else {
+			System.out.println("다름");
+		}
+		if(student2 == student3) {
+			System.out.println("같습");
+		}else {
+			System.out.println("다름");
+		}
+		
+		 student2 = (Student)ctx.getBean("student2");
+		System.out.println(student2.getName());
+		System.out.println(student2.getHobbys());
+		
 	}
 
 }
